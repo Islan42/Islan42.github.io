3be9653527429547273c380da59c35cf2a326a31
Islan <islan12victhor@gmail.com>
2024-01-27 06:00:59 -0300

    Criei as três funções principais

--- main.js
+++ main.js
@@ -18,14 +18,61 @@ function loadPaginaZero(){
 			</form>
 		</div>
 	`
+	
+	loadPaginaUm();
 }
 
 function loadPaginaUm(){
+	apagarConteudo(mainElement)
 	
+	mainElement.innerHTML = `
+		<div class="flex">
+			<img src="girassol.webp">
+			<div>
+				<p>Praticamente inofensivo.</p>
+			</div>
+		</div>
+	`
+	
+	loadPaginaDois();
+}
+
+function loadPaginaDois(){
+	apagarConteudo(mainElement);
+	
+	mainElement.innerHTML = `
+		<div class="flex">
+			<img src="girassol.webp">
+			<div>
+				<p>Olá. Meu nome é Islan Victhor, tenho 24 anos, e sou estudante de Ciência da Computação. Atualmente busco insersão na área de Desenvolvimento Web.</p>
+				
+				<h2>Habilidades e Conhecimentos</h2>
+				<ul>
+					<li>JavaScript</li>
+					<li>Express.Js</li>
+					<li>Vue</li>
+					<li>React</li>
+					<li>JQuery</li>
+					<li>PHP</li>
+					<li>Laravel</li>
+					<li>Python</li>
+					<li>Flask</li>
+					<li>C#</li>
+					<li>Docker</li>
+					<li>MySQL</li>
+					<li>MongoDB</li>
+				</ul>
+			</div>
+		</div>
+	`
 }
 
 function apagarConteudo(elemento){
 	while(elemento.firstChild){
 		elemento.removeChild(elemento.firstChild);
 	}
+}
+
+function carregar(tempo){
+	console.log('Loading')
 }
