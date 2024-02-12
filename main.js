ea79f27125f63d93327d7b5e9033ed617c8f21b3
Islan <islan12victhor@gmail.com>
2024-02-12 17:31:56 -0300

    Adicionei delay(), getJSONPromise, popup_senha() e statusDiv()

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let agente;
+let pessoa;
 
 homepage();
 
@@ -10,7 +11,7 @@ function homepage(){
 	
 	$('main').html(`
 		<div class="mx-auto max-w-xl  bg-zinc-300 mt-8 p-4">
-			<img src="public/SNI_logo_black.png" class="my-4 mx-auto h-48">
+			<img src="src/img/SNI_logo_black.png" class="my-4 mx-auto h-48">
 			<h2 class="text-center font-bold text-black">Cadastro Nacional de Pessoas Fuleiras</h2>
 			<form action="/" id="login" class="text-black mt-4 mx-auto max-w-80">
 				<div class="flex items-center">
@@ -19,8 +20,6 @@ function homepage(){
 			</form>
 		</div>
 	`);
-	
-	flashMessage('TESTE', 'RED');
 	
 	$('#agente').focus();
 	
@@ -37,7 +36,7 @@ function homepage(){
 		$('#agente').attr('disabled', true);
 		
 		await carregar(500)
-		$('main').css('background-image', 'url("public/SNI_logo_white.png")');
+		$('main').css('background-image', 'url("src/img/SNI_logo_white.png")');
 		$('main').addClass('bg-contain bg-no-repeat bg-center');
 		buscar_index();
 	})	
@@ -51,16 +50,15 @@ function buscar_index(){
 	
 	$('main').html(`
 		<div class="mx-auto max-w-xl  bg-zinc-300 mt-8 p-4">
-			<img src="public/SNI_logo_black.png" class="my-4 mx-auto h-48">
+			<img src="src/img/SNI_logo_black.png" class="my-4 mx-auto h-48">
 			<h2 class="text-center font-bold text-black">Cadastro Nacional de Pessoas Fuleiras</h2>
 			<form action="/" id="findPerson" class="text-black mt-4 mx-auto max-w-80">
 				<input type="text" id="nomePessoa" name="nomePessoa" placeholder="BUSCAR" class="px-2 py-1 w-full">
 			</form>
 		</div>
-		<span class="absolute top-8 left-0  text-xs">AGENT: ${agente}</span>
 	`);
 	
-	
+	statusDiv();
 	
 	$('#nomePessoa').focus();
 	
@@ -77,17 +75,19 @@ function buscar_index(){
 		$('#nomePessoa').attr('disabled', true);
 		
 		await carregar(1000)
-		if(nome.toUpperCase() === 'ISLAN VICTHOR'){
+		if(nome.toUpperCase() === 'ISLAN VICTHOR'){						
+			pessoa = await getJSONPromise('src/pessoas/islan_victhor.json');
 			await carregar(1000)
-			buscar_result()
+			buscar_result(pessoa)
 		} else {
+			await flashMessage('NENHUM RESULTADO ENCONTRADO', 'RED', 800);
 			$('#nomePessoa').attr('disabled', false);
 			$('#nomePessoa').focus();
 		}
 	})
 }
 
-function buscar_result(){
+function buscar_result(pessoa){
 	$('main').empty();
 	
 	$('main').html(`
@@ -96,7 +96,7 @@ function buscar_result(){
 				<img src="public/islan.png">
 			</div>
 			<div class="p-4">
-				<p>Praticamente inofensivo.</p>
+				<p>${pessoa.resumo}</p>
 			</div>
 		</div>
 	`)
@@ -104,6 +104,8 @@ function buscar_result(){
 	$('#command-line').attr('disabled', false);
 	$('#command-line').prop('value', '');
 	$('#command-line').focus();
+	
+	statusDiv();
 	
 	$('#command-line').keyup(function(event) {
 		const value = $(this).prop('value');
@@ -118,8 +120,13 @@ function buscar_result(){
 		$('#command-line').attr('disabled', true);
 		
 		if(comando.toUpperCase() === 'PROFILE'){
-			await carregar(2000);
-			profile();
+			popup_senha(pessoa.senha, async function(){
+				await carregar(2000);
+				profile(pessoa);				
+			}, function(){
+				$('#command-line').attr('disabled', false);
+				$('#command-line').focus();
+			});
 		} else {
 			$('#command-line').attr('disabled', false);
 			$('#command-line').focus();
@@ -127,7 +134,7 @@ function buscar_result(){
 	});
 }
 
-function profile(){
+function profile(pessoa){
 	$('main').empty();
 	
 	$('main').html(`
@@ -137,7 +144,7 @@ function profile(){
 			</div>
 			<div class="p-4  h-full overflow-auto">
 				<div class="p-4 bg-zinc-300">
-					<p>Olá. Meu nome é Islan Victhor, tenho 24 anos, e sou estudante de Ciência da Computação. Atualmente busco insersão na área de Desenvolvimento Web.</p>
+					<p>${pessoa.introducao}</p>
 				</div>
 				<div id="habilidades" class="bg-zinc-300 mt-2 p-2 text-black">
 					<h2 class="text-center">Habilidades e Conhecimentos</h2>
@@ -150,16 +157,16 @@ function profile(){
 		</div>
 	`)
 	
-	$.getJSON('habilidades.json', function(data){
-		$.each(data, function(i, habilidade){
-			$('#habilidades table tbody').append(`
-				<tr>
-					<td class="border border-black  p-1">${habilidade[0]}</td>
-					<td class="border border-black  p-1">${habilidade[1]}</td>
-				</tr>
-			`)
-		})
-	})
+	pessoa.habilidades.forEach((habilidade) => {
+		$('#habilidades table tbody').append(`
+			<tr>
+				<td class="border border-black  p-1">${habilidade[0]}</td>
+				<td class="border border-black  p-1">${habilidade[1]}</td>
+			</tr>
+		`);
+	});
+	
+	statusDiv();
 }
 
 async function carregar(tempo){
@@ -180,32 +187,85 @@ async function carregar(tempo){
 		}
 	}, intervalValue);
 	
-	await new Promise((resolve, reject) => {
-		setTimeout(()=>{
-			resolve()
-		}, tempo)		
-	})
+	await delay(tempo);
 	
 	clearInterval(interval);
 	
-	$('#msgCarregando').detach();
+	$('#msgCarregando').remove();
 }
 
-function flashMessage(msg, color){
+function flashMessage(msg, color, tempo){
 	const COLORS = {GREEN: 'bg-emerald-600', RED: 'bg-red-600'}
 	$('main').append(`
-		<div id="flashMessage" class="w-80 h-12 mt-8 leading-10 text-2xl text-white ${COLORS[color]}  border-4 border-double border-white  mx-auto text-center">
+		<div id="flashMessage" class="w-80 min-h-12 mt-8 leading-10 text-2xl text-white ${COLORS[color]}  border-4 border-double border-white  mx-auto text-center">
 			<p>${msg}</p>
 		</div>
 	`);
 	
-	awaitTime(1000).then(function() {		
+	return delay(tempo).then(function() {		
 		$('#flashMessage').remove();
 	})
 }
 
-function awaitTime(delay){
+function delay(tempo){
 	return new Promise((resolve, reject) => {
-		setTimeout(() => resolve(), delay);
+		setTimeout(() => resolve(), tempo);
 	})
+}
+
+function getJSONPromise(url){
+	return new Promise((resolve, reject) => {
+		$.getJSON(url, function(data){
+			resolve(data);
+		});
+	});
+}
+
+function popup_senha(senha, next, prev){
+	$('main').append(`
+		<div id="popupSenha" class="w-96 mt-8 mx-auto bg-zinc-300 border-4 border-double border-white p-2 text-black text-xl text-center relative">
+			<h2>DIGITE A SENHA DE ACESSO PARA PROSSEGUIR</h2>
+			<form id="formSenha">
+				<input type="password" id="senha" name="senha" placeholder="SENHA" class="p-2 mt-4 w-full">
+			</form>
+			<button id="closeBtn"class="absolute right-1 top-1 cursor-default"><img src="src/img/closebtn.png"></button>
+		</div>
+	`);
+	
+	$('#senha').focus();
+	
+	$('#formSenha').submit(function(event){
+		event.preventDefault();
+		
+		const res = $('#senha').val()
+		$('#senha').attr('disabled', true)
+		
+		if(res === senha){
+			next();
+		} else {
+			$('#senha').prop('value', '');
+			$('#senha').attr('disabled', false);
+			$('#senha').focus();
+		}
+	});
+	
+	$('#closeBtn').on('click', function(event){
+		$('#popupSenha').remove();
+		prev();
+	});
+	
+	$('#popupSenha').on('keydown', function(event){
+		if(event.key === "Escape"){
+			$('#popupSenha').remove();
+			prev();
+		}
+	});
+}
+
+function statusDiv(){
+	$('main').append(`
+		<div class="absolute top-8 left-0 text-xs bg-white/10 p-2">
+			<p>AGENT: ${agente}</p>
+		</div>
+	`);
 }
